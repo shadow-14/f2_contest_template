bf6d562572f4036b047c480dfc6e9375361fffe4
Sarthak Goyal <82680055+shadow-14@users.noreply.github.com>
2025-06-13 13:40:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ let arr = [
   { id: 3, name: "karen", age: "19", profession: "admin" },
 ];
 
+let arr2 = [
+  { id: 1, name: "john", age: "18", profession: "developer" },
+  { id: 2, name: "jack", age: "20", profession: "developer" },
+  { id: 3, name: "karen", age: "19", profession: "admin" },
+];
+
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
   let brr = arr.map((element)=>{if(element.profession=="developer")return element;
@@ -33,4 +39,6 @@ function removeAdmin() {
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr= arr.concat(arr2);
+  console.log(arr2);
 }
