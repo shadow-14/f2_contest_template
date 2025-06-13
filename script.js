a81a974980258e00524eb9b60ffe27cc98ece381
Sarthak Goyal <82680055+shadow-14@users.noreply.github.com>
2025-06-13 13:39:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,18 +8,27 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  let brr = arr.map((element)=>{if(element.profession=="developer")return element;
+                               else return null;})
+  console.log(brr);
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.foreach((element)=>{
+    if(element.profession==="developer")console.log(element)}
+    )
 }
 
 function addData() {
   //Write your code here, just console.log
+  arr.push({id:4, name:"susan", age:"20", profession:"intern"});
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  arr = arr.filter((item)=>item.profession!=="admin");
+  console.log(arr);
 }
 
 function concatenateArray() {
